f40997759e07ed4c60595366f54be0714585f139
Fabien <1061430320@qq.com>
2023-06-09 09:44:41 +0800

    normal

--- mdistiller/distillers/MDis.py
+++ mdistiller/distillers/MDis.py
@@ -266,7 +266,7 @@ class MDis(Distiller):
         #     self.rkd_angle_weight,
         # )
         # ! multi KD losses ! End #######
-        kd_sum = loss_dkd + loss_at + loss_rkd
+        loss_kd = loss_dkd + loss_at + loss_rkd
         # loss_kd = loss_dkd / kd_sum * loss_dkd + loss_at / kd_sum * loss_at + loss_rkd / kd_sum * loss_rkd
         # loss_kd = loss_dkd
         losses_dict = {
