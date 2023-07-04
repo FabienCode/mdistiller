aa8aa1dd06eaa46f47f73b192b3461ad54519d7d
Fabien <1061430320@qq.com>
2023-07-04 17:51:52 +0800

    normal

--- mdistiller/distillers/SRT.py
+++ mdistiller/distillers/SRT.py
@@ -135,7 +135,7 @@ class SRT(Distiller):
         # CrossKD
         # kd_logits = self.teacher.fc(nn.AvgPool2d(h)(feature_student["feats"][-1]).reshape(b, -1))\
         # with torch.no_grad():
-        kd_logits = self.student.fc(nn.AvgPool2d(h)(feature_teacher["feats"][-1]).reshape(b, -1))
+        kd_logits = self.student.fc(nn.AvgPool2d(h)(feature_teacher["feats"][-1]).reshape(b, -1)).detach()
         kd_loss_weight = 100
         # loss_kd = kd_loss_weight * kd_loss(kd_logits, logits_teacher, self.kd_temperature)
         loss_kd = kd_loss_weight * kd_loss(logits_student, kd_logits, self.kd_temperature)
