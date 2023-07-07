75655ddad53e9aa804ae4f298c7b7fe5f3d25849
Fabien <1061430320@qq.com>
2023-07-07 10:36:15 +0800

    normal

--- mdistiller/distillers/SRT.py
+++ mdistiller/distillers/SRT.py
@@ -55,11 +55,12 @@ class SRT(Distiller):
         loss_ce = ce_loss_weight * F.cross_entropy(logits_student, target)
 
         # CrossKD
-        s_feat = self.align_scale(feature_student["feats"][-1], feature_teacher["feats"][-1])
-        kd_logits = self.teacher.fc(nn.AvgPool2d(h)(s_feat).reshape(b, -1))
+        # s_feat = self.align_scale(feature_student["feats"][-1], feature_teacher["feats"][-1])
+        # kd_logits = self.teacher.fc(nn.AvgPool2d(h)(s_feat).reshape(b, -1))
+        kd_logits = self.teacher.fc(nn.AvgPool2d(h)(feature_student["feats"][-1]).reshape(b, -1))
         # with torch.no_grad():
         #     kd_logits = self.student.fc(nn.AvgPool2d(h)(feature_teacher["feats"][-1]).reshape(b, -1))
-        kd_loss_weight = 100
+        kd_loss_weight = 1
         # loss_kd = kd_loss_weight * kd_loss(kd_logits, logits_teacher, self.kd_temperature)
         loss_kd = kd_loss_weight * kd_loss(logits_student, kd_logits, self.kd_temperature)
 
