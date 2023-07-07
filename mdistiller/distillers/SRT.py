b05cf6f135bf2594e667a57b1ad9147eb26118cf
Fabien <1061430320@qq.com>
2023-07-07 15:55:26 +0800

    normal

--- mdistiller/distillers/SRT.py
+++ mdistiller/distillers/SRT.py
@@ -68,15 +68,15 @@ class SRT(Distiller):
         # s_feat = self.align_scale(feature_student["feats"][-1], feature_teacher["feats"][-1])
         # s_feat = self.conv_reg(s_feat)
         s_feat = self.conv_reg(feature_student["feats"][-1])
-        s_feat = self.align_scale(s_feat, feature_teacher["feats"][-1])
+        # s_feat = self.align_scale(s_feat, feature_teacher["feats"][-1])
         kd_logits = self.teacher.fc(nn.AvgPool2d(h)(s_feat).reshape(b, -1))
         # kd_logits = self.teacher.fc(nn.AvgPool2d(h)(feature_student["feats"][-1]).reshape(b, -1))
         # with torch.no_grad():
         #     kd_logits = self.student.fc(nn.AvgPool2d(h)(feature_teacher["feats"][-1]).reshape(b, -1))
         kd_loss_weight = 1
         # # loss_kd = kd_loss_weight * kd_loss(kd_logits, logits_teacher, self.kd_temperature)
-        loss_kd = kd_loss_weight * kd_loss(logits_student, kd_logits, self.kd_temperature)
-        # loss_kd = self.qkl_loss(logits_student, kd_logits)
+        # loss_kd = kd_loss_weight * kd_loss(logits_student, kd_logits, self.kd_temperature)
+        loss_kd = self.qkl_loss(logits_student, kd_logits)
 
         losses_dict = {
             "loss_ce": loss_ce,
