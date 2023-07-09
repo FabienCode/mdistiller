0d0fb269905c4b2efe1b88b8e87b66a301d14576
Fabien <1061430320@qq.com>
2023-07-09 12:21:25 +0800

    nromal

---
+++ mdistiller/distillers/CLD.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ._base import Distiller
+
+
+def kd_loss(logits_student, logits_teacher, temperature):
+    # mask = (torch.rand(logits_student.shape[0], logits_student.shape[1]) > 0.5).cuda()
+    log_pred_student = F.log_softmax(logits_student / temperature, dim=1)
+    pred_teacher = F.softmax(logits_teacher / temperature, dim=1)
+    loss_kd = F.kl_div(log_pred_student, pred_teacher, reduction="none").sum(1).mean()
+    # loss_kd = torch.where(mask==1, F.kl_div(log_pred_student, pred_teacher, reduction="none"), torch.zeros_like(logits_student)).sum(1).mean()
+    loss_kd *= temperature**2
+    return loss_kd
+
+def layer_kd_loss(logits_students, logits_teachers, temperature):
+    return sum([kd_loss(logits_student, logits_teacher, temperature) \
+                for logits_student, logits_teacher in zip(logits_students, logits_teachers)])
+
+
+class CLD(Distiller):
+    """Chain-of-Logits knowledge distillation via masked layers knowledge"""
+
+    def __init__(self, student, teacher, cfg):
+        super(CLD, self).__init__(student, teacher)
+        self.temperature = cfg.KD.TEMPERATURE
+        self.ce_loss_weight = cfg.KD.LOSS.CE_WEIGHT
+        self.kd_loss_weight = cfg.KD.LOSS.KD_WEIGHT
+        self.num_classes = student.fc.out_features
+        self.logits_avg = nn.Sequential(
+            nn.AvgPool2d(32),
+            nn.AvgPool2d(16),
+            nn.AvgPool2d(8)
+        )
+        # self.logits_bn = nn.BatchNorm2d(256)
+        # self.activation = nn.ReLU()
+        # self.activation = nn.ReLU6()
+
+        
+
+    def forward_train(self, image, target, **kwargs):
+        logits_student, feature_student = self.student(image)
+        # with torch.no_grad():
+        logits_teacher, feature_teacher = self.teacher(image)
+
+        # layers logits
+        # student logtis
+        bs = feature_teacher["feats"][0].shape[0]
+        channels = [feat.shape[1] for feat in feature_student["feats"]]
+
+        # add final avg pool feature
+        # mask version
+        # pooled_teacher_features = [(self.logits_avg[i](feature_teacher["feats"][i+1]).repeat(1, int(channels[-1]/channels[i+1]), 1, 1)\
+        #                                         .reshape(bs, -1)) for i in range(1,3)]
+        # pooled_teacher_features.append(feature_teacher["pooled_feat"])
+        # for i in range(len(pooled_teacher_features)):
+        #     pooled_teacher_features[i][torch.rand(8,100)>0.5] = 0
+        # pooled_student_features = [(self.logits_avg[i](feature_student["feats"][i+1]).repeat(1, int(channels[-1]/channels[i+1]), 1, 1)\
+        #                                         .reshape(bs, -1)) for i in range(1,3)]
+        # pooled_student_features.append(feature_student["pooled_feat"])
+        # for i in range(len(pooled_student_features)):
+        #     pooled_student_features[i][torch.rand(8,100)>0.5] = 0
+
+        # direct add and BN version
+        # pooled_teacher_features = [self.logits_bn(self.logits_avg[i]((feature_teacher["feats"][i+1]).repeat(1, int(channels[-1]/channels[i+1]), 1, 1))\
+        #                                          + feature_teacher["pooled_feat"].reshape(bs, -1, 1, 1)).reshape(bs, -1) for i in range(1,3)]
+        # pooled_teacher_features.append(feature_teacher["pooled_feat"])
+        # pooled_student_features = [self.logits_bn(self.logits_avg[i]((feature_student["feats"][i+1]).repeat(1, int(channels[-1]/channels[i+1]), 1, 1))\
+        #                                             + feature_student["pooled_feat"].reshape(bs, -1, 1, 1)).reshape(bs, -1) for i in range(1,3)]
+        # pooled_student_features.append(feature_student["pooled_feat"])
+
+        # with torch.no_grad():
+        #     tmp_fc = self.student.fc
+        #     logits_students = [tmp_fc(feat) for feat in pooled_student_features]
+        #     logits_teachers = [tmp_fc(feat) for feat in pooled_teacher_features]
+        #     for i in range(len(logits_students)-1):
+        #         mask = torch.rand(bs, self.num_classes) > 0.5
+        #         logits_students[i][mask] = 0
+        #         logits_teachers[i][mask] = 0
+        
+        logits_students = []
+        for i in range(len(feature_student["feats"][1:-1])):
+            with torch.no_grad():
+                tmp_s_fc = self.student.fc
+                logits_students.append(tmp_s_fc(self.logits_avg[i](feature_student["feats"][i+1]).repeat(1, int(channels[-1]/channels[i+1]), 1, 1)\
+                                                .reshape(bs, -1)))
+        logits_students.append(logits_student)
+        # teacher logits
+        logits_teachers = []
+        for i in range(len(feature_teacher["feats"][1:-1])):
+            with torch.no_grad():
+                tmp_t_fc = self.student.fc
+                logits_teachers.append(tmp_t_fc(self.logits_avg[i](feature_teacher["feats"][i+1]).repeat(1, int(channels[-1]/channels[i+1]), 1, 1)\
+                                                .reshape(bs, -1)))
+        logits_teachers.append(logits_teacher)
+        # add mask test
+        # for i in range(len(logits_students)-1):
+        #     mask = torch.rand(bs, self.num_classes) > 0.2
+        #     logits_students[i][mask] = 0
+        #     logits_teachers[i][mask] = 0
+        # losses
+        loss_ce = self.ce_loss_weight * F.cross_entropy(logits_student, target)
+        # loss_kd = self.kd_loss_weight * kd_loss(
+        #     logits_student, logits_teacher, self.temperature
+        # )
+        loss_kd_layers = self.kd_loss_weight * layer_kd_loss(logits_students, logits_teachers, self.temperature)
+
+        losses_dict = {
+            "loss_ce": loss_ce,
+            "loss_kd": loss_kd_layers,
+        }
+        return logits_student, losses_dict
---
+++ mdistiller/distillers/RKD.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ._base import Distiller
+
+
+def _pdist(e, squared, eps):
+    e_square = e.pow(2).sum(dim=1)
+    prod = e @ e.t()
+    res = (e_square.unsqueeze(1) + e_square.unsqueeze(0) - 2 * prod).clamp(min=eps)
+
+    if not squared:
+        res = res.sqrt()
+
+    res = res.clone()
+    res[range(len(e)), range(len(e))] = 0
+    return res
+
+
+def rkd_loss(f_s, f_t, squared=False, eps=1e-12, distance_weight=25, angle_weight=50):
+    stu = f_s.view(f_s.shape[0], -1)
+    tea = f_t.view(f_t.shape[0], -1)
+
+    # RKD distance loss
+    with torch.no_grad():
+        t_d = _pdist(tea, squared, eps)
+        mean_td = t_d[t_d > 0].mean()
+        t_d = t_d / mean_td
+
+    d = _pdist(stu, squared, eps)
+    mean_d = d[d > 0].mean()
+    d = d / mean_d
+
+    loss_d = F.smooth_l1_loss(d, t_d)
+
+    # RKD Angle loss
+    with torch.no_grad():
+        td = tea.unsqueeze(0) - tea.unsqueeze(1)
+        norm_td = F.normalize(td, p=2, dim=2)
+        t_angle = torch.bmm(norm_td, norm_td.transpose(1, 2)).view(-1)
+
+    sd = stu.unsqueeze(0) - stu.unsqueeze(1)
+    norm_sd = F.normalize(sd, p=2, dim=2)
+    s_angle = torch.bmm(norm_sd, norm_sd.transpose(1, 2)).view(-1)
+
+    loss_a = F.smooth_l1_loss(s_angle, t_angle)
+
+    loss = distance_weight * loss_d + angle_weight * loss_a
+    return loss
+
+def layers_rkd_loss(f_s, f_t, squared=False, eps=1e-12, distance_weight=25, angle_weight=50):
+    return sum([rkd_loss(s, t, squared, eps, distance_weight, angle_weight) for s, t in zip(f_s, f_t)])
+
+
+class RKD(Distiller):
+    """Relational Knowledge Disitllation, CVPR2019"""
+
+    def __init__(self, student, teacher, cfg):
+        super(RKD, self).__init__(student, teacher)
+        self.distance_weight = cfg.RKD.DISTANCE_WEIGHT
+        self.angle_weight = cfg.RKD.ANGLE_WEIGHT
+        self.ce_loss_weight = cfg.RKD.LOSS.CE_WEIGHT
+        self.feat_loss_weight = cfg.RKD.LOSS.FEAT_WEIGHT
+        self.eps = cfg.RKD.PDIST.EPSILON
+        self.squared = cfg.RKD.PDIST.SQUARED
+
+        self.logits_avg = nn.Sequential(
+            nn.AvgPool2d(32),
+            nn.AvgPool2d(16),
+            nn.AvgPool2d(8)
+        )
+
+    def forward_train(self, image, target, **kwargs):
+        logits_student, feature_student = self.student(image)
+        with torch.no_grad():
+            _, feature_teacher = self.teacher(image)
+
+        bs = feature_teacher["feats"][0].shape[0]
+        channels = []
+        for i in range(len(feature_student["feats"])):
+            channels.append(feature_student["feats"][i].shape[1])
+        # losses
+        loss_ce = self.ce_loss_weight * F.cross_entropy(logits_student, target)
+        kd_pooled_student = []
+        for i in range(len(feature_student["feats"][1:-1])):
+            kd_pooled_student.append(self.logits_avg[i](feature_student["feats"][i+1]).reshape(bs, -1))
+        kd_pooled_student.append(feature_student["pooled_feat"])
+        kd_pooled_teacher = []
+        for i in range(len(feature_teacher["feats"][1:-1])):
+            kd_pooled_teacher.append(self.logits_avg[i](feature_teacher["feats"][i+1]).reshape(bs, -1))
+        kd_pooled_teacher.append(feature_teacher["pooled_feat"])
+        loss_rkd = self.feat_loss_weight * layers_rkd_loss(
+            kd_pooled_student,
+            kd_pooled_teacher,
+            self.squared,
+            self.eps,
+            self.distance_weight,
+            self.angle_weight,
+        )
+        # loss_rkd = self.feat_loss_weight * rkd_loss(
+        #     feature_student["pooled_feat"],
+        #     feature_teacher["pooled_feat"],
+        #     self.squared,
+        #     self.eps,
+        #     self.distance_weight,
+        #     self.angle_weight,
+        # )
+        losses_dict = {
+            "loss_ce": loss_ce,
+            "loss_kd": loss_rkd,
+        }
+        return logits_student, losses_dict
