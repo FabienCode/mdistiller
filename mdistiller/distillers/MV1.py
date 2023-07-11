6eb2c9db15a5653ec619cea89e396223546f8de9
Fabien <1061430320@qq.com>
2023-07-11 22:34:38 +0800

    normal

--- mdistiller/distillers/MV1.py
+++ mdistiller/distillers/MV1.py
@@ -8,6 +8,7 @@ from mdistiller.engine.kd_loss import KDQualityFocalLoss, kd_loss, dkd_loss
 
 
 from mdistiller.engine.transformer_utils import MultiHeadAttention
+from torch.nn import Transformer
 
 
 class MV1(Distiller):
@@ -16,8 +17,8 @@ class MV1(Distiller):
     def __init__(self, student, teacher, cfg):
         super(MV1, self).__init__(student, teacher)
         self.ce_loss_weight = cfg.DKD.CE_WEIGHT
-        self.alpha = 8
-        self.beta = 1
+        self.alpha = 1
+        self.beta = 8
 
         self.temperature = cfg.DKD.T
         self.warmup = cfg.DKD.WARMUP
@@ -25,7 +26,8 @@ class MV1(Distiller):
         self.hint_layer = -1
         self.mask_per = 0.2
 
-        self.conv_reg = MultiHeadAttention(256, 4)
+        # self.conv_reg = MultiHeadAttention(256, 4)
+        self.conv_reg = Transformer(d_model=256, nhead=4, num_encoder_layers=3, num_decoder_layers=3, dim_feedforward=1024)
         # feat_s_shapes, feat_t_shapes = get_feat_shapes(
         #     self.student, self.teacher, cfg.FITNET.INPUT_SIZE
         # )
@@ -54,23 +56,26 @@ class MV1(Distiller):
         # 2. KD loss
         b, c, h, w = feature_student["feats"][self.hint_layer].shape
         f_s = feature_student["feats"][self.hint_layer].reshape(b, c, -1).transpose(2, 1).contiguous()
-        f_s, f_s_w = self.conv_reg(f_s, f_s, f_s)
-        f_s = f_s.transpose(2,1).reshape(b,c,h,w).contiguous()
-        weight_map = self.teacher.fc(nn.AvgPool2d(h)(f_s_w).reshape(b, -1))
-        sorted_indices = torch.argsort(weight_map, dim=1)
-        sorted_length = int(weight_map.shape[1] * self.mask_per)
-        top_indices = sorted_indices[:, : sorted_length]
-        mask = torch.zeros_like(weight_map).scatter_(1, top_indices, 1).bool()
+        f_t = feature_teacher["feats"][self.hint_layer].reshape(b, c, -1).transpose(2, 1).contiguous()
+        f_cross = self.conv_reg(f_s, f_t)
+
+        f_cross = f_cross.transpose(2,1).reshape(b,c,h,w).contiguous()
+
+        # weight_map = F.adaptive_avg_pool1d(nn.AvgPool2d(h)(f_s_w).reshape(b, -1))
+        # sorted_indices = torch.argsort(weight_map, dim=1)
+        # sorted_length = int(weight_map.shape[1] * self.mask_per)
+        # top_indices = sorted_indices[:, : sorted_length]
+        # mask = torch.zeros_like(weight_map).scatter_(1, top_indices, 1).bool()
+
         # CrossKD
-        kd_logits_s = self.teacher.fc(nn.AvgPool2d(h)(f_s).reshape(b, -1))
+        kd_logits_s = self.teacher.fc(nn.AvgPool2d(h)(f_cross).reshape(b, -1))
         loss_kd = min(kwargs["epoch"] / self.warmup, 1.0) * dkd_loss(
             kd_logits_s,
             logits_teacher,
             target,
             self.alpha,
             self.beta,
-            self.temperature,
-            mask
+            self.temperature
         )
 
         losses_dict = {
