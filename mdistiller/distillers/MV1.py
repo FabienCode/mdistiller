a3c1a83d9cf222def27525381afc10946f95bde8
Fabien <1061430320@qq.com>
2023-07-11 21:09:35 +0800

    normal

--- mdistiller/distillers/MV1.py
+++ mdistiller/distillers/MV1.py
@@ -18,6 +18,7 @@ class MV1(Distiller):
         self.ce_loss_weight = cfg.DKD.CE_WEIGHT
         self.alpha = 8
         self.beta = 1
+
         self.temperature = cfg.DKD.T
         self.warmup = cfg.DKD.WARMUP
 
