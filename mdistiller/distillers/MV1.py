1c7096cc444d7ba079b75baffe9fa371b282aacd
Fabien <1061430320@qq.com>
2023-07-14 12:16:49 +0800

    normal

--- mdistiller/distillers/MV1.py
+++ mdistiller/distillers/MV1.py
@@ -74,7 +74,8 @@ class MV1(Distiller):
             # "loss_feat": loss_feat
         }
         return logits_student, losses_dict
-    
+
+# test
 def aaloss(feature_student,
            feature_teacher,
            center_heat_map,
