86c7ea5c17b83b6f290160580faad40097d8099d
Fabien <1061430320@qq.com>
2023-07-20 11:29:52 +0800

    normal

--- mdistiller/distillers/MV1.py
+++ mdistiller/distillers/MV1.py
@@ -49,8 +49,8 @@ class MV1(Distiller):
         b, c, h, w = f_s.shape
         heat_map, wh, offset = self.conv_reg(f_s)
         aaloss_weight = 1
-        # * min(kwargs["epoch"] / self.warmup, 1.0)
-        loss_kd = aaloss_weight * aaloss(f_s, f_t, heat_map, wh, offset, k=8, kernel=3)
+        # 
+        loss_kd = aaloss_weight * min(kwargs["epoch"] / self.warmup, 1.0) * aaloss(f_s, f_t, heat_map, wh, offset, k=8, kernel=3)
         losses_dict = {
             "loss_ce": loss_ce,
             "loss_kd": loss_kd,
