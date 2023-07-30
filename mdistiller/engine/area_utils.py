e64a2e6964e8f5b34b97a51223aed2c71b76e675
Fabien <1061430320@qq.com>
2023-07-30 11:13:08 +0800

    normal

--- mdistiller/engine/area_utils.py
+++ mdistiller/engine/area_utils.py
@@ -27,12 +27,12 @@ class AreaDetection(nn.Module):
     def _build_head(in_channels, feat_channels, out_channels):
         layer = nn.Sequential(
             nn.Conv2d(in_channels, feat_channels, kernel_size=3, padding=1),
-            # nn.BatchNorm2d(feat_channels),
-            nn.InstanceNorm2d(feat_channels),
+            nn.BatchNorm2d(feat_channels),
+            # nn.InstanceNorm2d(feat_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(feat_channels, out_channels, kernel_size=1),
-            # nn.BatchNorm2d(out_channels),
-            nn.InstanceNorm2d(feat_channels),
+            nn.BatchNorm2d(out_channels),
+            # nn.InstanceNorm2d(feat_channels),
             nn.ReLU(inplace=True)
         )
         return layer
