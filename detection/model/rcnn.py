58a3ddc4c2cf8058480c429b1d64ad9b8d85f2a1
Fabien <1061430320@qq.com>
2023-08-02 19:55:02 +0800

    normal

--- detection/model/rcnn.py
+++ detection/model/rcnn.py
@@ -279,14 +279,17 @@ class RCNNKD(nn.Module):
             tmp_mask = s_fc_mask - t_fc_mask
             fc_mask = torch.zeros_like(tmp_mask)
             fc_mask[tmp_mask == 0] = 1
+            fc_mask[s_fc_mask == 0] = 0
+            fc_mask[t_fc_mask == 0] = 0
             detector_losses.update(reg_logits_loss(stu_predictions, tea_predictions, 4, fc_mask.bool()))
             # Region Loss
             mask, scores = extract_regions(f_s, heat_map, wh, offset, 8, 3)
             loss_regkd = 2 * aaloss(f_s, t_features[-1], mask, scores)
             losses['loss_regkd'] = loss_regkd
-            losses['loss_area_1'] = 1 * F.mse_loss(torch.cat((heat_map, wh, offset), dim=1),
-                                                   torch.cat((t_heat_map, t_wh, t_offset,), dim=1))
-            losses['loss_area_2'] = 1 * F.mse_loss(s_thresh, t_thresh)
+            losses['loss_area'] = 1 * F.mse_loss(torch.cat((heat_map, wh, offset), dim=1),
+                                                   torch.cat((t_heat_map, t_wh, t_offset,), dim=1)) \
+                                                    - 0.5 * torch.sum(s_thresh**2) \
+                                                    - 0.5 * torch.sum(t_thresh**2)
         else:
             raise NotImplementedError(self.kd_args.TYPE)
         if self.vis_period > 0:
