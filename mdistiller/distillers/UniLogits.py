cb4b464f2c4a67e2b070f7ba8d4717d0889d6207
Fabien <1061430320@qq.com>
2023-10-08 10:51:19 +0800

    latend_dim no BN LeakyReLU

--- mdistiller/distillers/UniLogits.py
+++ mdistiller/distillers/UniLogits.py
@@ -140,12 +140,12 @@ class featPro(nn.Module):
             # nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=2, padding=1),
             nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
             # nn.BatchNorm2d(in_channels),
-            # nn.LeakyReLU(inplace=True),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
+            # nn.ReLU(inplace=True),
             nn.Conv2d(in_channels, latent_dim, kernel_size=3, stride=1, padding=1),
             # nn.BatchNorm2d(in_channels),
-            # nn.LeakyReLU(inplace=True),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(inplace=True),
+            # nn.ReLU(inplace=True),
         )
         self.fc_mu = nn.Linear(latent_dim * size * size, latent_dim)
         self.fc_var = nn.Linear(latent_dim * size * size, latent_dim)
