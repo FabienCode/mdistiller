e186ee3e82ff254da319a9a22de849e3563a833d
fabien <1061430320@qq.com>
2023-10-27 10:35:03 +0800

    add mlkd config

--- mdistiller/dataset/imagenet.py
+++ mdistiller/dataset/imagenet.py
@@ -3,7 +3,8 @@ import numpy as np
 import torch
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
-
+from PIL import ImageOps, ImageEnhance, ImageDraw, Image
+import random
 
 data_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../data/imagenet')
 
@@ -17,6 +18,7 @@ class ImageNet(ImageFolder):
 class ImageNetInstanceSample(ImageNet):
     """: Folder datasets which returns (img, label, index, contrast_index):
     """
+
     def __init__(self, folder, transform=None, target_transform=None,
                  is_sample=False, k=4096):
         super().__init__(folder, transform=transform)
@@ -65,6 +67,197 @@ class ImageNetInstanceSample(ImageNet):
         else:
             return img, target, index
 
+
+class MultipleApply:
+    """Apply a list of transformations to an image and get multiple transformed images.
+
+    Args:
+        transforms (list or tuple): list of transformations
+
+    Example:
+
+        >>> transform1 = T.Compose([
+        ...     ResizeImage(256),
+        ...     T.RandomCrop(224)
+        ... ])
+        >>> transform2 = T.Compose([
+        ...     ResizeImage(256),
+        ...     T.RandomCrop(224),
+        ... ])
+        >>> multiply_transform = MultipleApply([transform1, transform2])
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image):
+        return [t(image) for t in self.transforms]
+
+
+def AutoContrast(img, _):
+    return ImageOps.autocontrast(img)
+
+
+def Brightness(img, v):
+    assert v >= 0.0
+    return ImageEnhance.Brightness(img).enhance(v)
+
+
+def Color(img, v):
+    assert v >= 0.0
+    return ImageEnhance.Color(img).enhance(v)
+
+
+def Contrast(img, v):
+    assert v >= 0.0
+    return ImageEnhance.Contrast(img).enhance(v)
+
+
+def Equalize(img, _):
+    return ImageOps.equalize(img)
+
+
+def Invert(img, _):
+    return ImageOps.invert(img)
+
+
+def Identity(img, v):
+    return img
+
+
+def Posterize(img, v):  # [4, 8]
+    v = int(v)
+    v = max(1, v)
+    return ImageOps.posterize(img, v)
+
+
+def Rotate(img, v):  # [-30, 30]
+    # assert -30 <= v <= 30
+    # if random.random() > 0.5:
+    #    v = -v
+    return img.rotate(v)
+
+
+def Sharpness(img, v):  # [0.1,1.9]
+    assert v >= 0.0
+    return ImageEnhance.Sharpness(img).enhance(v)
+
+
+def ShearX(img, v):  # [-0.3, 0.3]
+    # assert -0.3 <= v <= 0.3
+    # if random.random() > 0.5:
+    #    v = -v
+    return img.transform(img.size, Image.AFFINE, (1, v, 0, 0, 1, 0))
+
+
+def ShearY(img, v):  # [-0.3, 0.3]
+    # assert -0.3 <= v <= 0.3
+    # if random.random() > 0.5:
+    #    v = -v
+    return img.transform(img.size, Image.AFFINE, (1, 0, 0, v, 1, 0))
+
+
+def TranslateX(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    # assert -0.3 <= v <= 0.3
+    # if random.random() > 0.5:
+    #    v = -v
+    v = v * img.size[0]
+    return img.transform(img.size, Image.AFFINE, (1, 0, v, 0, 1, 0))
+
+
+def TranslateXabs(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    # assert v >= 0.0
+    # if random.random() > 0.5:
+    #    v = -v
+    return img.transform(img.size, Image.AFFINE, (1, 0, v, 0, 1, 0))
+
+
+def TranslateY(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    # assert -0.3 <= v <= 0.3
+    # if random.random() > 0.5:
+    #    v = -v
+    v = v * img.size[1]
+    return img.transform(img.size, Image.AFFINE, (1, 0, 0, 0, 1, v))
+
+
+def TranslateYabs(img, v):  # [-150, 150] => percentage: [-0.45, 0.45]
+    # assert 0 <= v
+    # if random.random() > 0.5:
+    #    v = -v
+    return img.transform(img.size, Image.AFFINE, (1, 0, 0, 0, 1, v))
+
+
+def Solarize(img, v):  # [0, 256]
+    assert 0 <= v <= 256
+    return ImageOps.solarize(img, v)
+
+
+def Cutout(img, v):  # [0, 60] => percentage: [0, 0.2] => change to [0, 0.5]
+    assert 0.0 <= v <= 0.5
+    if v <= 0.:
+        return img
+
+    v = v * img.size[0]
+    return CutoutAbs(img, v)
+
+
+def CutoutAbs(img, v):  # [0, 60] => percentage: [0, 0.2]
+    # assert 0 <= v <= 20
+    if v < 0:
+        return img
+    w, h = img.size
+    x0 = np.random.uniform(w)
+    y0 = np.random.uniform(h)
+
+    x0 = int(max(0, x0 - v / 2.))
+    y0 = int(max(0, y0 - v / 2.))
+    x1 = min(w, x0 + v)
+    y1 = min(h, y0 + v)
+
+    xy = (x0, y0, x1, y1)
+    color = (125, 123, 114)
+    # color = (0, 0, 0)
+    img = img.copy()
+    ImageDraw.Draw(img).rectangle(xy, color)
+    return img
+
+
+def augment_list():
+    l = [
+        (AutoContrast, 0, 1),
+        (Brightness, 0.05, 0.95),
+        (Color, 0.05, 0.95),
+        (Contrast, 0.05, 0.95),
+        (Equalize, 0, 1),
+        (Identity, 0, 1),
+        (Posterize, 4, 8),
+        (Rotate, -30, 30),
+        (Sharpness, 0.05, 0.95),
+        (ShearX, -0.3, 0.3),
+        (ShearY, -0.3, 0.3),
+        (Solarize, 0, 256),
+        (TranslateX, -0.3, 0.3),
+        (TranslateY, -0.3, 0.3)
+    ]
+    return l
+
+
+class RandAugment:
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m  # [0, 30] in fixmatch, deprecated.
+        self.augment_list = augment_list()
+
+    def __call__(self, img):
+        ops = random.choices(self.augment_list, k=self.n)
+        for op, min_val, max_val in ops:
+            val = min_val + float(max_val - min_val) * random.random()
+            img = op(img, val)
+        cutout_val = random.random() * 0.5
+        img = Cutout(img, cutout_val)  # for fixmatch
+        return img
+
+
 def get_imagenet_train_transform(mean, std):
     normalize = transforms.Normalize(mean=mean, std=std)
     train_transform = transforms.Compose(
@@ -76,6 +269,31 @@ def get_imagenet_train_transform(mean, std):
         ]
     )
     return train_transform
+
+
+def get_imagenet_train_transform_strong(mean, std):
+    normalize = transforms.Normalize(mean=mean, std=std)
+    train_transform_weak = transforms.Compose(
+        [
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+    train_transform_strong = transforms.Compose(
+        [
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            RandAugment(2, 10),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+
+    train_transform = MultipleApply([train_transform_weak, train_transform_strong])
+    return train_transform
+
 
 def get_imagenet_test_transform(mean, std):
     normalize = transforms.Normalize(mean=mean, std=std)
@@ -89,32 +307,47 @@ def get_imagenet_test_transform(mean, std):
     )
     return test_transform
 
+
 def get_imagenet_dataloaders(batch_size, val_batch_size, num_workers,
-    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+                             mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     train_transform = get_imagenet_train_transform(mean, std)
     train_folder = os.path.join(data_folder, 'train')
     train_set = ImageNet(train_folder, transform=train_transform)
     num_data = len(train_set)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, 
-        shuffle=True, num_workers=num_workers, pin_memory=True)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                               shuffle=True, num_workers=num_workers, pin_memory=True)
     test_loader = get_imagenet_val_loader(val_batch_size, mean, std)
     return train_loader, test_loader, num_data
 
-def get_imagenet_dataloaders_sample(batch_size, val_batch_size, num_workers, k=4096, 
-    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+
+def get_imagenet_dataloaders_strong(batch_size, val_batch_size, num_workers,
+                                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    train_transform = get_imagenet_train_transform_strong(mean, std)
+    train_folder = os.path.join(data_folder, 'train')
+    train_set = ImageNet(train_folder, transform=train_transform)
+    num_data = len(train_set)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                               shuffle=True, num_workers=num_workers, pin_memory=True)
+    test_loader = get_imagenet_val_loader(val_batch_size, mean, std)
+    return train_loader, test_loader, num_data
+
+
+def get_imagenet_dataloaders_sample(batch_size, val_batch_size, num_workers, k=4096,
+                                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     train_transform = get_imagenet_train_transform(mean, std)
     train_folder = os.path.join(data_folder, 'train')
     train_set = ImageNetInstanceSample(train_folder, transform=train_transform, is_sample=True, k=k)
     num_data = len(train_set)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, 
-        shuffle=True, num_workers=num_workers, pin_memory=True)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                               shuffle=True, num_workers=num_workers, pin_memory=True)
     test_loader = get_imagenet_val_loader(val_batch_size, mean, std)
     return train_loader, test_loader, num_data
+
 
 def get_imagenet_val_loader(val_batch_size, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     test_transform = get_imagenet_test_transform(mean, std)
     test_folder = os.path.join(data_folder, 'val')
     test_set = ImageFolder(test_folder, transform=test_transform)
     test_loader = torch.utils.data.DataLoader(test_set,
-        batch_size=val_batch_size, shuffle=False, num_workers=16, pin_memory=True)
+                                              batch_size=val_batch_size, shuffle=False, num_workers=16, pin_memory=True)
     return test_loader
