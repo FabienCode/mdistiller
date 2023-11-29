534d2dfc86bb602f32ae5dec824292effd7f0466
pavlov <1061430320@qq.com>
2023-11-29 18:52:02 +0800

    mvkd init

--- mdistiller/distillers/MVKD.py
+++ mdistiller/distillers/MVKD.py
@@ -70,7 +70,7 @@ class MVKD(Distiller):
         f_s = self.conv_reg(feature_student["feats"][self.hint_layer])
         f_t = feature_teacher["feats"][self.hint_layer]
 
-        if cur_epoch > 0:
+        if cur_epoch > 200:
             # 利用训练好的diffusion模型从随机噪声中生成不同的feature.
             f_new, f_inter = self.ddim_sampling(f_t) #
             # f_new = self.ddim_sample(f_t)
