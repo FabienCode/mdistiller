7543927be3c64de0aec609efcf24aa33387e7d7a
pavlov <1061430320@qq.com>
2023-12-06 20:37:36 +0800

    normal

--- mdistiller/distillers/MixKD.py
+++ mdistiller/distillers/MixKD.py
@@ -10,7 +10,7 @@ def kd_loss(logits_student, logits_teacher, temperature):
     log_pred_student = F.log_softmax(logits_student / temperature, dim=1)
     pred_teacher = F.softmax(logits_teacher / temperature, dim=1)
     loss_kd = F.kl_div(log_pred_student, pred_teacher, reduction="none").sum(1).mean()
-    loss_kd *= temperature**2
+    loss_kd *= temperature ** 2
     return loss_kd
 
 
@@ -42,51 +42,69 @@ class MixKD(Distiller):
 
     def forward_train(self, image_weak, image_strong, target, **kwargs):
         logits_student_weak, feature_student_weak = self.student(image_weak)
-        logits_student_strong, feature_student_strong = self.student(image_strong)
         with torch.no_grad():
             logits_teacher_weak, feature_teacher_weak = self.teacher(image_weak)
             logits_teacher_strong, feature_teacher_strong = self.teacher(image_strong)
 
         # losses
-        loss_ce = self.ce_loss_weight * (F.cross_entropy(logits_student_strong, target) + F.cross_entropy(logits_student_weak, target))
+        loss_ce = self.ce_loss_weight * F.cross_entropy(logits_student_weak, target)
         f_s_weak = self.conv_reg(feature_student_weak["feats"][self.hint_layer])
-        f_s_strong = self.conv_reg(feature_student_strong["feats"][self.hint_layer])
 
         f_t_weak = feature_teacher_weak["feats"][self.hint_layer]
         f_t_strong = feature_teacher_strong["feats"][self.hint_layer]
+        s_t_weak = calculate_saliency(f_t_weak)
+        s_t_strong = calculate_saliency(f_t_strong)
+        mix_t_weak, mix_t_strong = mix_features(f_t_weak, f_t_strong, s_t_weak, s_t_strong)
 
-        # mix_f_t_weak, lma = mix_feature(f_t_strong, f_t_weak, self.beta)
-        # loss_feat_weak = F.mse_loss(f_s_weak, mix_f_t_weak) + F.mse_loss(f_s_weak, f_t_strong) * lma + F.mse_loss(f_s_weak, f_t_weak) * (1 - lma)
-        loss_feat = F.mse_loss(f_s_weak, f_t_weak) + F.mse_loss(f_s_strong, f_t_strong) + \
-            F.mse_loss(f_s_weak, f_t_strong) + F.mse_loss(f_s_strong, f_t_weak)
+        loss_feat = F.mse_loss(f_s_weak, mix_t_weak) + F.mse_loss(f_s_weak, mix_t_strong)
         losses_dict = {
             "loss_ce": loss_ce,
             "loss_feat_weak": self.feat_loss_weight * loss_feat
         }
-        return logits_student_strong, losses_dict
-
-def mix_feature(f_w, f_s, beta):
-    beta_distribution = torch.distributions.beta.Beta(beta, beta)
-    lma = beta_distribution.sample()
-    bbx1, bby1, bbx2, bby2 = rand_bbox(f_w.size(), lma)
-    f_w[:, :, bbx1:bbx2, bby1:bby2] = f_s[:, :, bbx1:bbx2, bby1:bby2]
-    return f_w, lma
+        return logits_student_weak, losses_dict
 
 
-def rand_bbox(size, lam):
-    W = size[2]
-    H = size[3]
-    cut_rat = torch.sqrt(1. - lam)
-    cut_w = torch.tensor(W, dtype=torch.float32).mul(cut_rat).type(torch.int)
-    cut_h = torch.tensor(H, dtype=torch.float32).mul(cut_rat).type(torch.int)
+def calculate_saliency(feature_map):
+    saliency = F.adaptive_avg_pool2d(feature_map, 1)
+    return saliency
 
-    # uniform
-    cx = torch.randint(0, W, (1,)).item()
-    cy = torch.randint(0, H, (1,)).item()
+def mix_features(f_a, f_b, s_a, s_b):
+    weights_a = s_a / (s_a + s_b)
+    weights_b = s_b / (s_a + s_b)
 
-    bbx1 = torch.clamp(cx - cut_w // 2, 0, W)
-    bby1 = torch.clamp(cy - cut_h // 2, 0, H)
-    bbx2 = torch.clamp(cx + cut_w // 2, 0, W)
-    bby2 = torch.clamp(cy + cut_h // 2, 0, H)
+    w_a_expand = weights_a.expand_as(f_a)
+    w_b_expand = weights_b.expand_as(f_b)
 
-    return bbx1, bby1, bbx2, bby2
+    mix_f_a = w_a_expand * f_a + (1 - w_a_expand) * f_b
+    mix_f_b = w_b_expand * f_b + (1 - w_b_expand) * f_a
+    return mix_f_a, mix_f_b
+
+# def mix_feature(x, y, alpha):
+#     if alpha > 0.:
+#         beta_distribution = torch.distributions.beta.Beta(alpha, alpha)
+#         lma = beta_distribution.sample()
+#     else:
+#         lam = 1.
+#
+#     bbx1, bby1, bbx2, bby2 = rand_bbox(f_w.size(), lma)
+#     f_w[:, :, bbx1:bbx2, bby1:bby2] = f_s[:, :, bbx1:bbx2, bby1:bby2]
+#     return f_w, lma
+#
+#
+# def rand_bbox(size, lam):
+#     W = size[2]
+#     H = size[3]
+#     cut_rat = torch.sqrt(1. - lam)
+#     cut_w = torch.tensor(W, dtype=torch.float32).mul(cut_rat).type(torch.int)
+#     cut_h = torch.tensor(H, dtype=torch.float32).mul(cut_rat).type(torch.int)
+#
+#     # uniform
+#     cx = torch.randint(0, W, (1,)).item()
+#     cy = torch.randint(0, H, (1,)).item()
+#
+#     bbx1 = torch.clamp(cx - cut_w // 2, 0, W)
+#     bby1 = torch.clamp(cy - cut_h // 2, 0, H)
+#     bbx2 = torch.clamp(cx + cut_w // 2, 0, W)
+#     bby2 = torch.clamp(cy + cut_h // 2, 0, H)
+#
+#     return bbx1, bby1, bbx2, bby2
