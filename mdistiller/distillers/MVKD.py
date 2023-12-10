4e3c7dccc21336308a5b0cea52f747acdf16ca42
Fabien <1061430320@qq.com>
2023-12-10 17:54:27 +0800

    advance code

--- mdistiller/distillers/MVKD.py
+++ mdistiller/distillers/MVKD.py
@@ -101,26 +101,19 @@ class MVKD(Distiller):
         f_t = feature_teacher["feats"][self.hint_layer]
 
         if cur_epoch > self.first_rec_kd:
-            if self.use_condition:
-                diffusion_f_t = self.ddim_sample(f_t, conditional=logits_student)
-            else:
-                diffusion_f_t = self.ddim_sample(f_t)
+            diffusion_f_t = self.ddim_sample(f_t, conditional=logits_student) if self.use_condition else self.ddim_sample(f_t)
             if self.diff_num > 1:
                 for i in range(self.diff_num - 1):
-                    if self.use_condition:
-                        diffusion_f_t += self.ddim_sample(f_t, conditional=logits_student)
-                    else:
-                        diffusion_f_t += self.ddim_sample(f_t)
+                    diffusion_f_t += self.ddim_sample(f_t, conditional=logits_student) if self.use_condition else self.ddim_sample(f_t)
             diffusion_f_t /= self.diff_num
             mvkd_loss = self.mvkd_weight * F.mse_loss(f_s, diffusion_f_t)
             fitnet_loss = self.feat_loss_weight * F.mse_loss(f_s, f_t)
             loss_kd = mvkd_loss + fitnet_loss
         else:
             x_feature_t, noise, t = self.prepare_diffusion_concat(f_t)
-            if self.use_condition:
-                rec_feature_t = self.rec_module(x=x_feature_t.float(), t=t, conditional=logits_student)
-            else:
-                rec_feature_t = self.rec_module(x_feature_t.float(), t)
+            rec_feature_t = self.rec_module(x=x_feature_t.float(), t=t,
+                                            conditional=logits_student) if self.use_condition else self.rec_module(
+                x_feature_t.float(), t)
             rec_loss = self.rec_weight * F.mse_loss(rec_feature_t, f_t)
             # rec_noise = self.rec_module(x_feature_t.float(), t)
             # rec_loss = self.rec_weight * F.mse_loss(rec_noise, noise)
