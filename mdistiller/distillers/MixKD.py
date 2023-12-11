00598626c5c20677fdba2d7fd8538f6f8df587c1
pavlov <1061430320@qq.com>
2023-12-11 17:18:08 +0800

    mixkd saliency bbox normal

--- mdistiller/distillers/MixKD.py
+++ mdistiller/distillers/MixKD.py
@@ -64,14 +64,16 @@ class MixKD(Distiller):
         head_map_t_s, wh_t_s, offset_t_s = self.saliency_det(f_t_s)
         saliency_t_w_b, _ = saliency_bbox(heat_map_t_w, wh_t_w, offset_t_w, 1, 3)
         saliency_t_s_b, _ = saliency_bbox(head_map_t_s, wh_t_s, offset_t_s, 1, 3)
+        f_t_w_aug = f_t_w.clone()
+        f_t_s_aug = f_t_s.clone()
         for i in range(saliency_t_w_b.shape[1]):
             saliency_tmp_w = saliency_t_w_b[:, i, :].clone()
             saliency_tmp_s = saliency_t_s_b[:, i, :].clone()
-            f_t_w, f_t_s = aug_feat(f_t_w, f_t_s, saliency_tmp_w, saliency_tmp_s)
+            f_t_w_aug, f_t_s_aug = aug_feat(f_t_w_aug, f_t_s_aug, saliency_tmp_w, saliency_tmp_s)
         # loss_kd = kd_loss(logits_student_weak, logits_teacher_weak, 4) + kd_loss(
         #     logits_student_weak, logits_teacher_strong, 4
         # )
-        loss_feat_aug = F.mse_loss(f_s_w, f_t_w) + F.mse_loss(f_s_w, f_t_s)
+        loss_feat_aug = F.mse_loss(f_s_w, f_t_w_aug) + F.mse_loss(f_s_w, f_t_s_aug)
         loss_feat = loss_feat_aug
         losses_dict = {
             "loss_ce": loss_ce,
