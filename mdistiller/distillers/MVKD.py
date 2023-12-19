b191accb6b7d93d437ebee5bb24b073807cba579
Fabien <1061430320@qq.com>
2023-12-19 19:00:40 +0800

    crd_mvkd res324_84 config

--- mdistiller/distillers/MVKD.py
+++ mdistiller/distillers/MVKD.py
@@ -156,9 +156,8 @@ class MVKD(Distiller):
             context_embd = self.clip_model.get_text_features(**code_inputs)
         # diff_con = torch.concat((context_embd, logits_student_weak), dim=-1)
         diff_con = context_embd
-        # if cur_epoch > self.first_rec_kd:
-        if cur_epoch % 2 == 1:
-
+        if cur_epoch > self.first_rec_kd:
+        # if cur_epoch % 2 == 1:
             mvkd_loss = 0.
             diffusion_f_t = 0.
             for i in range(self.diff_num):
