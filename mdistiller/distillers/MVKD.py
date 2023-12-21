58a36e7b328042ca4b6f406fcfede60fda213126
pavlov <1061430320@qq.com>
2023-12-21 11:30:55 +0800

    reconstructed multi-view

--- mdistiller/distillers/MVKD.py
+++ mdistiller/distillers/MVKD.py
@@ -152,7 +152,7 @@ class MVKD(Distiller):
         temp_text = 'A reconstructed feature map of '
         code_tmp = []
         for i in range(b):
-            code_tmp.append(temp_text + CIFAR100_Labels[target[i].item()] + 'from different views.')
+            code_tmp.append(temp_text + CIFAR100_Labels[target[i].item()] + ' from different views.')
         with torch.no_grad():
             code_inputs = self.clip_processor(text=code_tmp, return_tensors="pt", padding=True).to(device)
             context_embd = self.clip_model.get_text_features(**code_inputs)
