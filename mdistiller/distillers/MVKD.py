4f0c086c457baf0703eee19a770694104b44a111
pavlov <1061430320@qq.com>
2023-12-25 10:16:40 +0800

    mvkd v3

--- mdistiller/distillers/MVKD.py
+++ mdistiller/distillers/MVKD.py
@@ -84,7 +84,7 @@ class MVKD(Distiller):
 
         t_b, t_c, t_w, t_h = feat_t_shapes[self.hint_layer]
         self.use_condition = cfg.MVKD.DIFFUSION.USE_CONDITION
-        self.rec_module = Model(ch=t_c, out_ch=t_c, ch_mult=(1, 2), num_res_blocks=2, attn_resolutions=[t_w//2, t_w],
+        self.rec_module = Model(ch=t_c, out_ch=t_c, ch_mult=(1, 2), num_res_blocks=2, attn_resolutions=[t_w],
                                 in_channels=t_c, resolution=t_w, dropout=0.0, use_condition=self.use_condition,
                                 condition_dim=self.condition_dim)
         # self.rec_module = Model(ch=t_c*2, out_ch=t_c, ch_mult=(1, 2, 4), num_res_blocks=1, attn_resolutions=[4, 8],
@@ -312,9 +312,17 @@ def at_loss(g_s, g_t, p):
 def multi_loss(logits_student_weak, logits_teacher_weak,
                logits_student_strong, logits_teacher_strong,
                mask, weight):
-    loss_kd_weak = (weight * ((kd_loss(logits_student_weak, logits_teacher_weak, 4) * mask).mean()))
+    loss_kd_weak = (weight * ((kd_loss(logits_student_weak, logits_teacher_weak, 4) * mask).mean() +
+                              (kd_loss(logits_student_weak, logits_teacher_weak, 2) * mask).mean() +
+                              (kd_loss(logits_student_weak, logits_teacher_weak, 3) * mask).mean() +
+                              (kd_loss(logits_student_weak, logits_teacher_weak, 5) * mask).mean() +
+                              (kd_loss(logits_student_weak, logits_teacher_weak, 6) * mask).mean()))
 
-    loss_kd_strong = (weight * ((kd_loss(logits_student_strong, logits_teacher_strong, 4) * mask).mean()))
+    loss_kd_strong = (weight * ((kd_loss(logits_student_strong, logits_teacher_strong, 4) * mask).mean() +
+                                (kd_loss(logits_student_strong, logits_teacher_strong, 2) * mask).mean() +
+                                (kd_loss(logits_student_strong, logits_teacher_strong, 3) * mask).mean() +
+                                (kd_loss(logits_student_strong, logits_teacher_strong, 5) * mask).mean() +
+                                (kd_loss(logits_student_strong, logits_teacher_strong, 6) * mask).mean()))
     return loss_kd_weak + loss_kd_strong
 
 
