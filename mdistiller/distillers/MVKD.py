1ce64d26c3a7005feb29269e2eb068d07d93a29a
pavlov <1061430320@qq.com>
2023-12-27 10:43:32 +0800

    normal

--- mdistiller/distillers/MVKD.py
+++ mdistiller/distillers/MVKD.py
@@ -85,7 +85,7 @@ class MVKD(Distiller):
 
         t_b, t_c, t_w, t_h = feat_t_shapes[self.hint_layer]
         self.use_condition = cfg.MVKD.DIFFUSION.USE_CONDITION
-        self.rec_module = Model(ch=t_c, out_ch=t_c, ch_mult=(1, 1), num_res_blocks=2, attn_resolutions=[t_w],
+        self.rec_module = Model(ch=t_c, out_ch=t_c, ch_mult=(1, 1), num_res_blocks=1, attn_resolutions=[t_w],
                                 in_channels=t_c, resolution=t_w, dropout=0.0, use_condition=self.use_condition,
                                 condition_dim=self.condition_dim)
         latent_dim = t_c
