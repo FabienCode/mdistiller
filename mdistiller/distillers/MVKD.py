3c3f56f1a1de2c1be5b83d3d6936f01898a91a60
fabien <1061430320@qq.com>
2024-01-03 00:16:44 +0800

    normal

--- mdistiller/distillers/MVKD.py
+++ mdistiller/distillers/MVKD.py
@@ -288,21 +288,26 @@ def at_loss(g_s, g_t, p):
 def multi_loss(logits_student_weak, logits_teacher_weak,
                logits_student_strong, logits_teacher_strong,
                mask, weight):
-    # loss_kd_weak = (weight * ((kd_loss(logits_student_weak, logits_teacher_weak, 4) * mask).mean() +
-    #                           (kd_loss(logits_student_weak, logits_teacher_weak, 2) * mask).mean() +
-    #                           (kd_loss(logits_student_weak, logits_teacher_weak, 3) * mask).mean() +
-    #                           (kd_loss(logits_student_weak, logits_teacher_weak, 5) * mask).mean() +
-    #                           (kd_loss(logits_student_weak, logits_teacher_weak, 6) * mask).mean()))
+    loss_kd_weak = (weight * ((kd_loss(logits_student_weak, logits_teacher_weak, 4) * mask).mean() +
+                              (kd_loss(logits_student_weak, logits_teacher_weak, 2) * mask).mean() +
+                              (kd_loss(logits_student_weak, logits_teacher_weak, 3) * mask).mean() +
+                              (kd_loss(logits_student_weak, logits_teacher_weak, 5) * mask).mean() +
+                              (kd_loss(logits_student_weak, logits_teacher_weak, 6) * mask).mean()))
 
-    loss_kd_weak = (weight * ((kd_loss(logits_student_weak, logits_teacher_weak, 4) * mask).mean()))
+    loss_kd_strong = (weight * ((kd_loss(logits_student_strong, logits_teacher_strong, 4) * mask).mean() +
+                                (kd_loss(logits_student_strong, logits_teacher_strong, 2) * mask).mean() +
+                                (kd_loss(logits_student_strong, logits_teacher_strong, 3) * mask).mean() +
+                                (kd_loss(logits_student_strong, logits_teacher_strong, 5) * mask).mean() +
+                                (kd_loss(logits_student_strong, logits_teacher_strong, 6) * mask).mean()))
+    # loss_kd_weak = (weight * ((kd_loss(logits_student_weak, logits_teacher_weak, 4) * mask).mean()))
 
-    loss_kd_strong = (weight * ((kd_loss(logits_student_strong, logits_teacher_strong, 4) * mask).mean()))
+    # loss_kd_strong = (weight * ((kd_loss(logits_student_strong, logits_teacher_strong, 4) * mask).mean()))
 
     loss_cc_weak = (weight * ((cc_loss(logits_student_weak, logits_teacher_weak, 4) * mask).mean()))
 
     loss_bc_weak = (weight * ((bc_loss(logits_student_weak, logits_teacher_weak, 4) * mask).mean()))
 
-    return loss_kd_weak + loss_kd_strong + loss_cc_weak + loss_bc_weak
+    return loss_kd_weak + loss_kd_strong
 
 
 def determine_article(word):
