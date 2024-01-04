ab815fdc8f7e2fcc3daf54f9e362d348412befae
pavlov <1061430320@qq.com>
2024-01-04 11:35:24 +0800

    add new mkd

--- mdistiller/distillers/MVKD.py
+++ mdistiller/distillers/MVKD.py
@@ -338,7 +338,7 @@ def multi_loss(logits_student_weak, logits_teacher_weak,
                                 (bc_loss(logits_student_strong, logits_teacher_strong, 5) * mask).mean() +
                                 (bc_loss(logits_student_strong, logits_teacher_strong, 6) * mask).mean()))
 
-    return loss_kd_weak + loss_kd_strong + loss_cc_weak + loss_bc_weak
+    return loss_kd_weak + loss_kd_strong + loss_cc_weak + loss_bc_weak + loss_cc_strong + loss_bc_strong
 
 
 def determine_article(word):
