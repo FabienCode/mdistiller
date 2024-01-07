fbc41fca67ebcfbe76b0ca4caf2d7187b65b8834
fabien <1061430320@qq.com>
2024-01-07 11:03:17 +0800

    normal

--- mdistiller/distillers/MVKD.py
+++ mdistiller/distillers/MVKD.py
@@ -148,7 +148,7 @@ class MVKD(Distiller):
 
             # A reconstructed feature map of a medium-sized, red turtle
             # code_tmp.append(temp_text + size_choice + ", " + color_choice + " " + CIFAR100_Labels[target[i].item()])
-            code_tmp.append(temp_text + CIFAR100_Labels[target[i].item()])
+            code_tmp.append(temp_text + CIFAR100_Labels[target[i].item()] + ".")
         with torch.no_grad():
             code_inputs = self.clip_processor(text=code_tmp, return_tensors="pt", padding=True).to(device)
             context_embd = self.clip_model.get_text_features(**code_inputs)
