1ac7d47a13e4966c0d9d6aa4b39cde6a26b96d33
pavlov <1061430320@qq.com>
2024-01-25 12:11:40 +0800

    add ReviewKD DFKD config

--- mdistiller/engine/trainer.py
+++ mdistiller/engine/trainer.py
@@ -331,7 +331,7 @@ class AugTrainer(BaseTrainer):
         batch_size = image_weak.size(0)
         acc1, acc5 = accuracy(preds, target, topk=(1, 5))
         train_meters["losses"].update(loss.cpu().detach().numpy().mean(), batch_size)
-        train_meters["losses_ce"].update(loss_ce.cpu().detach().numpy().mean(), batch_size)
+        train_meters["loss_ce"].update(loss_ce.cpu().detach().numpy().mean(), batch_size)
         train_meters["top1"].update(acc1[0], batch_size)
         train_meters["top5"].update(acc5[0], batch_size)
         # print info
